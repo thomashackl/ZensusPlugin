9beb8aaae201da890c73a9dee342211a62515b6d
Thomas Hackl <thomas.hackl@uni-passau.de>
2014-05-12 11:24:00 +0200

    bind to button instead of form

--- assets/javascript/unizensusplugin.js
+++ assets/javascript/unizensusplugin.js
@@ -1,7 +1,7 @@
 STUDIP.UnizensusPlugin = {
 
     init: function() {
-        $('form[name="edit_template_form"]').bind('submit', function(event) {
+        $('button[name="submit"]').bind('click', function(event) {
             var success = true;
             var nameInput = $('input[name="name"]');
             if (nameInput.val() == '') {
@@ -17,6 +17,9 @@ STUDIP.UnizensusPlugin = {
             if (msgInput.val() == '') {
                 msgInput.after('<span class="messagebox_error">Bitte geben Sie einen Nachrichtentext für die Vorlage an!</span>');
                 success = false;
+            }
+            if (!success) {
+                event.preventDefault();
             }
             return success;
         });
