97915a61893058e80908451a0b6ec1b175e76988
Thomas Hackl <thomas.hackl@uni-passau.de>
2014-05-12 09:40:25 +0200

    better form

--- templates/edit_template.php
+++ templates/edit_template.php
@@ -4,19 +4,19 @@
         <label class="caption" for="name">
             <?= _('Name') ?>
         </label>
-        <input type="text" name="name" size="50" maxlength="255" value="<?= $tpl->name ?>" placeholder="<?= _('Geben Sie hier einen Namen für die Vorlage ein.') ?>" required="required"/>
+        <input type="text" name="name" size="75" maxlength="255" value="<?= $tpl->name ?>" placeholder="<?= _('Geben Sie hier einen Namen für die Vorlage ein.') ?>" required="required"/>
     </div>
     <div>
         <label class="caption" for="subject">
             <?= _('Betreff') ?>
         </label>
-        <input type="text" name="subject" size="50" maxlength="255" value="<?= $tpl->subject ?>" placeholder="<?= _('Geben Sie hier eine Betreffzeile ein.') ?>" required="required"/>
+        <input type="text" name="subject" size="75" maxlength="255" value="<?= $tpl->subject ?>" placeholder="<?= _('Geben Sie hier eine Betreffzeile ein.') ?>" required="required"/>
     </div>
     <div>
         <label class="caption" for="message">
             <?= _('Nachrichtentext') ?>
         </label>
-        <textarea name="message" cols="40" rows="10" placeholder="<?= _('Geben Sie hier einen Text für die zu verschickende Nachricht bzw. zu erstellende Ankündigung ein.') ?>" required="required"><?= $tpl->message ?></textarea>
+        <textarea name="message" cols="90" rows="15" placeholder="<?= _('Geben Sie hier einen Text für die zu verschickende Nachricht bzw. zu erstellende Ankündigung ein.') ?>" required="required"><?= $tpl->message ?></textarea>
     </div>
     <div class="submit_wrapper">
         <?php if ($tpl) { ?>
